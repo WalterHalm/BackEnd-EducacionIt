af37a6f57ca1ae1fc081c3bf15fe2e2db0e6ea78
Walter Halm <walter_halm@hotmail.com>
2023-11-06 10:37:57 -0300

    configuracion mongo atras para render

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export default{
-    secret: "SantaGula"
+    secret: "SantaGula",
+    mongoUrlLocal:"mongodb://127.0.0.1:27017/RotiseriaSantaGula",
+    mongoUrlAtlas:"mongodb://walterhalm:1nx35xTk2fGlu4lQ@ac-c29jtzq-shard-00-00.gi3ocih.mongodb.net:27017,ac-c29jtzq-shard-00-01.gi3ocih.mongodb.net:27017,ac-q-shard-00-02.gi3ocih.mongodb.net:27017/?ssl=true&replicaSet=atlas-hhl4qr-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
+
--- src/database.js
+++ src/database.js
@@ -1,7 +1,10 @@
 import mongoose from "mongoose"
+import config from "./config.js"
 
 
-mongoose.connect("mongodb://127.0.0.1:27017",{
+
+
+mongoose.connect(config.mongoUrlAtlas,{
                 keepAlive:true,
                 useNewUrlParser:true,
                 useUnifiedTopology: true,
